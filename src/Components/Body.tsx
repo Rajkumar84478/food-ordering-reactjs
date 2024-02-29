69db63dbb3961c840488de0092c7ab75b8ca7b2d
Rajkumar Chauhan <rajkumar.chauhan@rajkumar.chauhan-C02ZQ08MMD6P>
2024-02-29 14:04:40 +0530

    before the route

--- src/Components/Body.tsx
+++ src/Components/Body.tsx
@@ -1,22 +1,25 @@
 import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { cardData } from "./CardData";
+import Shimmer from "./Shimmer";
+// import { cardData } from "./CardData";
 // import { cardData } from "./CardData";
 
 function Body() {
   // const cardsobject:any=JSON.parse(cardData);
   // console.log(cardsobject);
-//   const [items, setItems] = useState(cardData.items);
+  //   const [items, setItems] = useState(cardData.items);
   const [items, setItems] = useState([]);
+  const [filterItems,setFilterItems]=useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   interface cardTypes {
-    id?: string,
+    id?: string;
     name: string;
-    areaName:string,
-    cloudinaryImageId:string,
-    costForTwo:string,
-    cuisines: string[],
-    avgRating:number,
+    areaName: string;
+    cloudinaryImageId: string;
+    costForTwo: string;
+    cuisines: string[];
+    avgRating: number;
   }
 
   useEffect(() => {
@@ -28,28 +31,56 @@ function Body() {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7429763&lng=77.117685&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const jsonData = await responseData.json();
-    const cardsData=jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
-    console.log(cardsData);
+    const cardsData =
+      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    // console.log(cardsData);
     setItems(cardsData);
-
+    setFilterItems(cardsData);
   };
 
   function filterList() {
     const filterData: any = items.filter(
       (card: any) => card.info.avgRating > 4
     );
-    setItems(filterData);
+    setFilterItems(filterData);
   }
 
-  return (
+  return items.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+          />
+
+          <button
+            onClick={() => {   
+              const filterCards:any=items.filter((item:any)=>{
+            //   console.log(item.info.name.toLowerCase())
+              return item.info.name.toLowerCase().includes(searchValue.toLowerCase());
+              }
+            )
+            // console.log(filterCards);
+            setFilterItems(filterCards);
+            }}
+          >
+            search
+          </button>
+        </div>
         <button className="filter-btn" onClick={() => filterList()}>
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {items.map((data: any) => (
+        {filterItems.map((data: any) => (
           <RestaurantCard key={data.info.id} resData={data.info} />
         ))}
       </div>
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,11 @@
 // import React from 'react'
+import { useState } from 'react';
 import logo from '/Users/rajkumar.chauhan/Vscode/Practice/practive/reactPractice/app/src/images/logo.png'
 
 export default function Header() {
+  
+  const [btnName,setBtnName]=useState("Login");
+
   return (
     <div className='header'>
         <div className="logo-container">
@@ -16,6 +20,9 @@ export default function Header() {
           <li>About Us</li>
           <li>Constact Us</li>
           <li>Cart</li>
+          <button onClick={()=>{
+            setBtnName(btnName==='Login'?'Logout':'Login');
+          }} className='login'>{btnName}</button>
         </ul>
 
         </div>
